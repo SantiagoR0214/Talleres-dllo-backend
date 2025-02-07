69a7c0fbd2dbd0e54ecceeb6b4aeb1f9b576544f
SantiagoR0214 <99304628+SantiagoR0214@users.noreply.github.com>
2025-02-07 18:44:42 -0500

    Update Taller1.js

---
+++ Taller1.js
@@ -0,0 +1,20 @@
+
+function grados(a,b) {
+    return b = (a*(9/5))+32 ;
+}
+
+
+console.log(grados(50)); 
+
+function cuadraticamas (a,b,c) {
+    return x =(-b+(((b**2)-(4*a*c))**(1/2))/2*a) ;
+
+}
+
+console.log(cuadraticamas(1,5,4)); 
+
+function cuadraticamenos (a,b,c) {
+        return x =(-b-(((b**2)-(4*a*c))**(1/2))/2*a);
+}
+
+console.log(cuadraticamenos(1,5,4)); 
