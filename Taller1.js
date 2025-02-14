c6a598b5f14f6643a216e4a3a30f29e95ca94d50
SantiagoR0214 <99304628+SantiagoR0214@users.noreply.github.com>
2025-02-14 01:36:30 -0500

    Update Taller1.js

--- Taller1.js
+++ Taller1.js
@@ -1,20 +1,69 @@
-
-function grados(a,b) {
-    return b = (a*(9/5))+32 ;
+function convertidorTemp(gradCel){
+    tempFaren = gradCel * (9/5) + 32
+    return tempFaren
 }
 
+console.log(convertidorTemp(25)); 
 
-console.log(grados(50)); 
+function resolvedor(b, a, c, val){
+    if (val== true){
+        x= (-b + (b**2 - 4*(a*c))**(1/2)) / 2*a
+    }
+    else{
+        x= (-b - (b**2 - 4*(a*c))**(1/2)) / 2*a
+    }
 
-function cuadraticamas (a,b,c) {
-    return x =(-b+(((b**2)-(4*a*c))**(1/2))/2*a) ;
-
+    return x
 }
 
-console.log(cuadraticamas(1,5,4)); 
+console.log(resolvedor(5,1,4, true)); 
 
-function cuadraticamenos (a,b,c) {
-        return x =(-b-(((b**2)-(4*a*c))**(1/2))/2*a);
+
+function mejorParidad(x){
+    if (x % 2 == 0){
+        respuesta= "Numero Par"
+    }
+    else{
+        respuesta = "Numero Impar"
+    }
+    return respuesta
 }
 
-console.log(cuadraticamenos(1,5,4)); 
+console.log(mejorParidad(5)); 
+
+
+function peorParidad(num) {
+    if(num ==1){
+        paridad = "1 es impar"
+    }
+    if(num ==2){
+        paridad = "2 es par"
+    }
+    if(num ==3){
+        paridad = "3 es impar"
+    }
+    if(num ==4){
+        paridad = "4 es par"
+    }
+    if(num ==5){
+        paridad = "5 es impar"
+    }
+    if(num ==6){
+        paridad = "6 es par"
+    }
+    if(num ==7){
+        paridad = "7 es impar"
+    }
+    if(num ==8){
+        paridad = "8 es par"
+    }
+    if(num ==9){
+        paridad = "9 es impar"
+    }
+    if(num ==10){
+        paridad = "10 es par"
+    }
+    return paridad
+}
+
+console.log(peorParidad(5)); 
